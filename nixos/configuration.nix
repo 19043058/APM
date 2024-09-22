d84d14306a8562e1a4d8076224924e7ca74ed597
Ardemium <contact@ardemium.nl>
2024-09-22 13:19:15 +0200

    added a force into the .nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,11 +1,11 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   # Bootloader configuration
   boot.loader.grub.devices = [ "/dev/sda" ]; # Replace with your actual device
 
   # Filesystem configuration
-  fileSystems."/" = {
+  fileSystems."/" = lib.mkForce {
     device = "/dev/sda1"; # Replace with your actual root partition
   };
 
