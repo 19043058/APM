3a5105bcd2e85119a0d001a4838a63d24e75f76b
Ardemium <contact@ardemium.nl>
2024-09-22 02:05:29 +0200

    rm double tls

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -14,9 +14,8 @@ terraform {
 }
 
 provider "proxmox" {
-  pm_tls_insecure = true
   pm_api_url          = var.proxmox_url
   pm_api_token_id     = var.proxmox_token_id
   pm_api_token_secret = var.proxmox_token_secret
-  pm_tls_insecure     = false   # Enforce TLS verification
+  pm_tls_insecure     = true   # Enforce TLS verification
 }
