43ee0bf75c24b335098363c865e212470814e5e2
Ardemium <contact@ardemium.nl>
2024-09-22 21:54:35 +0200

    deploy

--- terraform/modules/vm/main.tf
+++ terraform/modules/vm/main.tf
@@ -7,17 +7,13 @@ resource "proxmox_vm_qemu" "ubuntu_vm" {
   memory      = 2048                     # Memory in MB
   cores       = 2                        # Number of CPU cores
   sockets     = 1                        # Number of CPU sockets
-  pool        = "default"                # (Optional) Specify resource pool
-
-  # Network configuration using Cloud-Init
-  ipconfig0   = "ip=dhcp"                # Assign IP address dynamically via DHCP
 
   # Cloud-Init specific options
   ciuser     = "ubuntu"                  # Cloud-init username
-  cipassword = "password"                # Cloud-init password
-  sshkeys    = <<EOF
-  ssh-rsa AABB3NzaC1kj...your-ssh-public-key
-  EOF
+  cipassword = "password"                # Cloud-init password (replace with a secure password)
+
+  # Network configuration using Cloud-Init
+  ipconfig0   = "ip=dhcp"                # Assign IP address dynamically via DHCP
 
   # Define disk settings (Cloud-Init disk will be auto-configured)
   disks {
@@ -32,6 +28,7 @@ resource "proxmox_vm_qemu" "ubuntu_vm" {
 
   # Network interface for the VM
   network {
+    model  = "virtio"                    # Network card model (virtio is recommended)
     bridge = "vmbr0"                     # Proxmox bridge (usually vmbr0)
   }
 
@@ -40,20 +37,4 @@ resource "proxmox_vm_qemu" "ubuntu_vm" {
 
   # Optional: Boot settings (VM will boot from the disk created via the template)
   boot = "order=scsi0"
-
-  # Optional: Remote-exec provisioner to run post-provisioning commands
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt-get update",
-      "sudo apt-get install -y nginx",
-    ]
-
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = file("~/.ssh/id_rsa")   # Replace with the path to your SSH private key
-      host        = self.ipv4_address
-      port        = 22
-    }
-  }
 }
