d267928e64f60a14ed44f08589217eb405f9d773
Ardemium <contact@ardemium.nl>
2024-09-22 02:02:40 +0200

    set tls insecure for dev

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -14,6 +14,7 @@ terraform {
 }
 
 provider "proxmox" {
+  pm_tls_insecure = true
   pm_api_url          = var.proxmox_url
   pm_api_token_id     = var.proxmox_token_id
   pm_api_token_secret = var.proxmox_token_secret
