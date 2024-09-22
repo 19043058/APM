c8547731475d52345bbfbfed0a203cff170ea310
Ardemium <contact@ardemium.nl>
2024-09-22 19:57:22 +0200

    changed module

--- terraform/modules/vm/main.tf
+++ terraform/modules/vm/main.tf
@@ -1,35 +1,35 @@
-resource "proxmox_vm_qemu" "vm" {
-  name        = var.vm_name
-  vmid        = var.vm_id
-  memory      = var.vm_memory
-  cores       = var.vm_cores
-  sockets     = 1
-  scsihw      = "virtio-scsi-pci"
-  boot        = "cdn"
-  bootdisk    = "scsi0"
-  agent       = 1
-  onboot      = true
-  os_type     = "cloud-init"
-  target_node = var.node_name
-
-  disk {
-    slot     = "scsi0"
-    size     = var.vm_disk_size
-    type     = "disk"
-    storage  = "local-lvm"
-    iothread = true
+# Define a Proxmox QEMU Virtual Machine
+resource "proxmox_vm_qemu" "ubuntu_vm" {
+  name        = "ubuntu-vm"
+  target_node = "ardemium"  # Specify the Proxmox node where the ISO is stored
+  memory      = 2048        # Memory in MB
+  cores       = 2           # Number of CPU cores
+  sockets     = 1           # Number of CPU sockets
+  cpu         = "host"      # CPU type
+  
+  # Define disk configuration with local ISO
+  disks {
+    ide {
+      ide2 {
+        cdrom {
+          iso = "local:iso/ubuntu-24.04.1-live-server-amd64.iso"  # Path to your ISO on node 'ardemium'
+        }
+      }
+    }
   }
 
+  # Define network configuration
   network {
-    model    = "virtio"
-    bridge   = var.network_bridge
+    bridge   = "vmbr0"  # Proxmox bridge
+    model    = "virtio" # Use virtio for better performance
+    firewall = false    # Disable firewall for simplicity
   }
 
-  clone = var.template_name
+  # Boot settings
+  boot = "order=ide2"
 
-  lifecycle {
-    ignore_changes = [
-      network[0].macaddr  # Ignore changes to MAC address
-    ]
-  }
+  # Additional optional settings
+  onboot = true         # Start VM automatically after Proxmox boot
+  agent  = 1            # Enable QEMU agent for guest integration
+  bios   = "seabios"    # Use seabios as BIOS
 }
