fad723476ceb99001cf3e14e4611b89559c8d268
Ardemium <contact@ardemium.nl>
2024-09-22 22:53:22 +0200

    nossh

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,10 +8,8 @@ module "my_vm" {
   sockets          = 1     # Optional; default is 1
   ipconfig0        = "ip=dhcp"  # Optional; default is "ip=dhcp"
   nameserver       = "8.8.8.8"  # Optional; default is "8.8.8.8"
-  sshkeys          = file("~/.ssh/id_rsa.pub")
   ciuser           = "ubuntu"   # Optional; default is "ubuntu"
   cipassword       = "yourpassword"  # Optional; sensitive
-  ssh_private_key  = file("~/.ssh/id_rsa")
   ssh_user         = "ubuntu"   # Optional; default is "ubuntu"
   storage          = "local-lvm"  # Optional; default is "local-lvm"
   disk_size        = "30G"      # Optional; default is "20G"
