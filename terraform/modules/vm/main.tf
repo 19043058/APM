e6d6f315c187c24042b9134f07c9c4766e47b4ab
Ardemium <contact@ardemium.nl>
2024-09-22 22:41:14 +0200

    dedfgxsdfghsdf

--- terraform/modules/vm/main.tf
+++ terraform/modules/vm/main.tf
@@ -1,15 +1,38 @@
-# Define a Proxmox QEMU Virtual Machine using the Cloud-Init template
-resource "proxmox_vm_qemu" "ubuntu_vm" {
-  name        = "ubuntu-vm"              # VM name
-  target_node = "ardemium"               # Proxmox node name
-  clone       = "ubuntu-cloud-init-template"  # Clone the Cloud-Init template VM
+resource "proxmox_vm_qemu" "ubuntu_cloud_init_vm" {
+  name        = "ubuntu-cloud-init-vm"
+  target_node = "proxmox-node1"             # Proxmox node
+  clone       = "ubuntu-cloud-init-template" # Clone from a cloud-init template
 
-  storage = "local"
-  cores   = 3
-  sockets = 1
-  memory  = 2560
-  disk_gb = 4
-  nic     = "virtio"
-  bridge  = "vmbr0"
-  os_type   = "cloud-init"
+  # VM Hardware Configuration
+  storage  = "local-lvm"   # Storage pool
+  cores    = 2             # Number of CPU cores
+  sockets  = 1             # Number of CPU sockets
+  memory   = 2048          # Memory in MB
+  disk_gb  = 10            # Disk size in GB
+
+  # Cloud-Init Configuration
+  ciuser     = "ubuntu"             # Cloud-init user
+  cipassword = "password"           # User password
+  ipconfig0  = "ip=dhcp"            # Dynamic IP address
+
+  sshkeys = <<EOF
+  ssh-rsa AAAAB3...GeneratedPublicKey
+  EOF
+
+  # Network Interface
+  network {
+    bridge = "vmbr0"                # Network bridge
+    model  = "virtio"               # Network interface model
+  }
+
+  # Cloud-Init Disk
+  disks {
+    ide {
+      ide2 {
+        cloudinit {
+          storage = "local-lvm"      # Cloud-Init storage
+        }
+      }
+    }
+  }
 }
